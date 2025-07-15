adb22af09f83d742d4cb68115b904d165844e2cc
westjin <uko802@naver.com>
2025-07-15 16:37:07 +0900

    레포지토리 이름 변경

--- src/main/java/org/example/config/RootConfig.java
+++ src/main/java/org/example/config/RootConfig.java
@@ -1,9 +1,43 @@
 package org.example.config;
 
 
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
+@EnableTransactionManagement
 public class RootConfig {
 
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://mysql:3306/springdb?useSSL=false&serverTimezone=Asia/Seoul");
+        ds.setUsername("springuser"); // ← 너가 설정한 MySQL 계정
+        ds.setPassword("springpw");   // ← 너가 설정한 MySQL 비밀번호
+        return ds;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+        factory.setDataSource(dataSource());
+        return factory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
 }
+
