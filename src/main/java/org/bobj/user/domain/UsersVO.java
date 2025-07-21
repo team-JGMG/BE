e642d3bb837951ea6f5c1456bd9cefd4bc0a285e
GuBon <rnqhsrud01@gmail.com>
2025-07-21 15:29:41 +0900

    [FEAT]#7 create UsersVO, SocialLoginsVO

---
+++ src/main/java/org/bobj/user/domain/UsersVO.java
@@ -0,0 +1,27 @@
+package org.bobj.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersVO {
+
+    private Long userId;          // user_id BIGINT
+    private String name;          // name VARCHAR(100)
+    private String email;         // email VARCHAR(255)
+    private String gender;        // gender CHAR(1)
+    private String nickname;      // nickname VARCHAR(50)
+    private String ssn;           // ssn VARCHAR(20) (주민등록번호)
+    private String phone;         // phone VARCHAR(20)
+    private Date createdAt;       // created_at DATETIME
+    private Date updatedAt;       // updated_at DATETIME
+    private boolean isAdmin;      // is_admin BOOLEAN
+    private String bankCode;      // bank_code VARCHAR(50)
+    private String accountNumber; // account_number VARCHAR(50)
+
+}
