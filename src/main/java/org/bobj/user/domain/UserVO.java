baaf7cb2bee69caeb0ff56ceaac61aec9f6983fd
GuBon <rnqhsrud01@gmail.com>
2025-07-23 17:36:05 +0900

    [FEAT]#18 change name to UserVO.java

--- src/main/java/org/bobj/user/domain/UserVO.java
+++ src/main/java/org/bobj/user/domain/UserVO.java
@@ -1,20 +1,21 @@
 package org.bobj.user.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsersVO {
+public class UserVO {
 
     private Long userId;          // user_id BIGINT
     private String name;          // name VARCHAR(100)
     private String email;         // email VARCHAR(255)
-    private String gender;        // gender CHAR(1)
     private String nickname;      // nickname VARCHAR(50)
     private String ssn;           // ssn VARCHAR(20) (주민등록번호)
     private String phone;         // phone VARCHAR(20)
@@ -23,5 +24,6 @@ public class UsersVO {
     private boolean isAdmin;      // is_admin BOOLEAN
     private String bankCode;      // bank_code VARCHAR(50)
     private String accountNumber; // account_number VARCHAR(50)
+
 
 }
