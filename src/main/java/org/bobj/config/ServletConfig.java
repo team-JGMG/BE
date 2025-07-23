5db104dc8c2e37fa64aea5b207f9c5ac35c5f54a
seul <angel3030303@gmail.com>
2025-07-23 10:49:20 +0900

    A [FEAT] #25 Add property list, detail, and delete APIs

--- src/main/java/org/bobj/config/ServletConfig.java
+++ src/main/java/org/bobj/config/ServletConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {"org.bobj.controller"})  // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
 @ComponentScan(basePackages = {
         "org.bobj.common.exception",
         "org.bobj.controller",
@@ -21,6 +20,7 @@ import org.springframework.web.servlet.view.JstlView;
         "org.bobj.share.controller",
         "org.bobj.user.controller"})
 public class ServletConfig implements WebMvcConfigurer {
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/org/bobj/property/domain/PropertyVO.java
+++ src/main/java/org/bobj/property/domain/PropertyVO.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
