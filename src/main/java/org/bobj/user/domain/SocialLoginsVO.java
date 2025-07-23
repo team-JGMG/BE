7070e19fce905542374b182a4c5a18e9edd39351
GuBon <rnqhsrud01@gmail.com>
2025-07-23 17:48:25 +0900

    [FEAT]#18 create builder, delete accessToken in SocialLoginsVO.java

--- src/main/java/org/bobj/user/domain/SocialLoginsVO.java
+++ src/main/java/org/bobj/user/domain/SocialLoginsVO.java
@@ -1,6 +1,7 @@
 package org.bobj.user.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,10 +16,18 @@ public class SocialLoginsVO {
     private Long userId;          // user_id BIGINT (FK)
     private String provider;      // provider VARCHAR(20) (e.g., "kakao", "naver")
     private String providerId;    // provider_id VARCHAR(255) (소셜 서비스의 고유 ID)
-    private String accessToken;   // access_token TEXT
     private String refreshToken;  // refresh_token TEXT
     private Date tokenExpiresAt;  // token_expires_at DATETIME
     private String profileData;   // profile_data JSON (JSON 문자열 그대로 저장)
     private Date createdAt;       // created_at DATETIME
 
+    @Builder
+    public SocialLoginsVO(Long userId, String provider, String providerId, String refreshToken, String profileData) {
+        this.userId = userId;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.refreshToken = refreshToken;
+        this.profileData = profileData;
+        this.createdAt = new Date();
+    }
 }
