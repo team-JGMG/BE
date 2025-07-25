6e32158012756e7be345e529df30246127980b4c
GuBon <rnqhsrud01@gmail.com>
2025-07-25 11:33:18 +0900

    [FEAT]#18 add TokenDTO constructor in TokenDTO.java

--- src/main/java/org/bobj/user/dto/TokenDTO.java
+++ src/main/java/org/bobj/user/dto/TokenDTO.java
@@ -10,4 +10,13 @@ import lombok.Getter;
 public class TokenDTO {
     private String accessToken;
     private String refreshToken;
+    private Boolean isAdmin;    // 편의를 위해 유지
+    private String role;        // 추가
+
+    public TokenDTO(String accessToken, String refreshToken, boolean isAdmin) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.isAdmin = isAdmin;
+        this.role = isAdmin ? "ADMIN" : "USER";  // role 자동 설정
+    }
 }
