4d1d2042986631041756e48703aa827d6dc03dce
westjin <uko802@naver.com>
2025-08-04 00:45:47 +0900

    [FEAT] #63 Update point charge request flow after payment verified

---
+++ src/main/java/org/bobj/payment/repository/PaymentRepository.java
@@ -0,0 +1,24 @@
+package org.bobj.payment.repository;
+
+
+import lombok.RequiredArgsConstructor;
+import org.bobj.payment.domain.PaymentVO;
+import org.bobj.payment.mapper.PaymentMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentRepository {
+    private final PaymentMapper paymentMapper;
+
+    public boolean existsByImpUid(String impUid){
+        return paymentMapper.existsByImpUid(impUid);
+    }
+    public void save(PaymentVO paymentVO){
+        paymentMapper.save(paymentVO);
+    }
+    public PaymentVO findByImpUid(String impUid){
+        return paymentMapper.findByImpUid(impUid);
+    }
+
+}
