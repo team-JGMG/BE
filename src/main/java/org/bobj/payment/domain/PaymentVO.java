74c965ee66c7a59f25ce13341831681fbb7e8cd4
westjin <uko802@naver.com>
2025-08-04 00:40:29 +0900

    [FEAT] #63 Add PaymentVO

--- src/main/java/org/bobj/payment/domain/PaymentVO.java
+++ src/main/java/org/bobj/payment/domain/PaymentVO.java
@@ -1,24 +1,19 @@
 package org.bobj.payment.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class PaymentVO {
-    private Long paymentId;
+    private String impUid;
+    private String merchantUid;
     private Long userId;
     private BigDecimal amount;
     private PaymentStatus status;
-    private String pgTid;
-    private String qrUrl;
-    private LocalDateTime createdAt;
-    private LocalDateTime endAt;
+    private LocalDateTime paidAt;
+//    private LocalDateTime createdAt;
 }
