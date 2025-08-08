acae82138c6f3560f5206fd2e62b471358bfb061
westjin <uko802@naver.com>
2025-08-08 23:25:34 +0900

    [FiX] add import

--- src/main/java/org/bobj/point/controller/PointController.java
+++ src/main/java/org/bobj/point/controller/PointController.java
@@ -1,5 +1,6 @@
 package org.bobj.point.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
