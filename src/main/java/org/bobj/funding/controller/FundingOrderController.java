b4a208820bce4b96243675d6abd5dd6a9b5d01d0
seul <angel3030303@gmail.com>
2025-08-12 10:06:02 +0900

    [REFACTOR] #280 Modify end point

--- src/main/java/org/bobj/funding/controller/FundingOrderController.java
+++ src/main/java/org/bobj/funding/controller/FundingOrderController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/api/funding-order")
+@RequestMapping("/api/auth/funding-order")
 @RequiredArgsConstructor
 @Log4j2
 @Api(tags="펀딩 주문 API")
--- src/main/java/org/bobj/property/controller/PropertyController.java
+++ src/main/java/org/bobj/property/controller/PropertyController.java
@@ -18,14 +18,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/property")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @Log4j2
 @Api(tags="매물 API")
 public class PropertyController {
     private final PropertyService propertyService;
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(
+            value = "/auth/property",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ApiOperation(value = "매물 등록", notes = "새로운 매물을 등록합니다. 법정동코드가 있으면 자동으로 전월세 데이터를 조회하여 월세 정보를 설정합니다.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "매물 등록 성공"),
@@ -53,7 +55,7 @@ public class PropertyController {
     }
 
 
-    @GetMapping
+    @GetMapping("/auth/property")
     @ApiOperation(value = "매물 목록 조회", notes = "[관리자] 요약 정보가 담긴 매물 목록을 반환합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "category", value = "카테고리 필터명(pending -> 대기중 ,approved -> 승인 ,failed -> 펀딩 실패)", defaultValue = "pending", dataType = "string", paramType = "query"),
@@ -73,7 +75,7 @@ public class PropertyController {
         return ResponseEntity.ok(ApiCommonResponse.createSuccess(list));
     }
 
-    @GetMapping("/user/{userId}")
+    @GetMapping("/auth/property/user/{userId}")
     @ApiOperation(value = "사용자 매물 목록 조회", notes = "[매도자] 특정 사용자가 등록한 매물 목록을 조회합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "사용자 ID", required = true, dataType = "long", paramType = "path"),
@@ -97,7 +99,7 @@ public class PropertyController {
         return ResponseEntity.ok(ApiCommonResponse.createSuccess(response));
     }
 
-    @GetMapping("/{propertyId}")
+    @GetMapping("/auth/property/{propertyId}")
     @ApiOperation(value = "매물 상세 조회", notes = "[관리자, 매도자]특정 매물의 상세 정보를 반환합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "propertyId", value = "매물 ID", required = true, dataType = "long", paramType = "path")
@@ -113,7 +115,7 @@ public class PropertyController {
         return ResponseEntity.ok(ApiCommonResponse.createSuccess(result));
     }
 
-    @PatchMapping("/{propertyId}/status")
+    @PatchMapping("/auth/property/{propertyId}/status")
     @ApiOperation(value = "매물 상태 변경", notes = "매물을 승인 또는 거절합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "propertyId", value = "매물 ID", required = true, dataType = "long", paramType = "path"),
@@ -132,7 +134,7 @@ public class PropertyController {
         return ResponseEntity.ok(ApiCommonResponse.createSuccess("매물 상태 변경 완료"));
     }
 
-    @GetMapping("/sold")
+    @GetMapping("/property/sold")
     @ApiOperation(value = "매각 완료 매물 목록 조회", notes = "[메인] 매각 완료 매물들을 불러옵니다.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "매각 완료 매물 목록 조회 성공", response = PropertySoldResponseDTO.class, responseContainer = "List"),
@@ -177,7 +179,7 @@ public class PropertyController {
 
 
     //매각 처리 테스트용 api
-    @PostMapping("/sold-process")
+    @PostMapping("/property/sold-process")
     public ResponseEntity<String> processSoldProperties() {
         log.info("매각 처리 로직을 수동으로 시작합니다.");
         try {
